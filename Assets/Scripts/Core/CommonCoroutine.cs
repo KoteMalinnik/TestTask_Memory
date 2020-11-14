2ec3c35ff626de711e903f64de00b3ab94a0db00
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 00:16:35 +0300

    add basic core scripts

---
+++ Assets/Scripts/Core/CommonCoroutine.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// Класс корутины без параметров, который содержит:
+/// -информацию о том, работает ли корутина в данный момент;
+/// -действие по завершению, на которое надо подписываться;
+/// -методы для запуска и остановки корутины.
+/// </summary>
+public sealed class CommonCoroutine
+{
+    #region Events
+	
+    /// <summary>
+    /// Действие, которое необходимо выполнить по завершению корутины.
+    /// </summary>
+    public event Action OnFinish = null;
+	
+    #endregion
+
+    #region Fields
+	
+    private Coroutine coroutine = null;
+	
+    #endregion
+
+    #region Properties
+	
+    private MonoBehaviour owner { get; } = null;
+    private Func<IEnumerator> routine { get; } = null;
+
+    /// <summary>
+    /// Вернет true, если корутина в данный момент выполняется.
+    /// </summary>
+    public bool isRunning => coroutine != null;
+	
+    #endregion
+
+	#region Constructors
+	
+    public CommonCoroutine(MonoBehaviour owner, Func<IEnumerator> routine)
+    {
+        if (owner == null || routine == null) throw new ArgumentNullException();
+
+        this.owner = owner;
+        this.routine = routine;
+    }
+
+	#endregion
+	
+	#region Public Methods
+	
+    /// <summary>
+    /// Запустить выполнение корутины.
+    /// </summary>
+    public void Start()
+    {
+        Stop();
+        coroutine = owner.StartCoroutine(Process());
+    }
+
+    /// <summary>
+    /// Остановить выполнение корутины.
+    /// </summary>
+    public void Stop()
+    {
+        if (isRunning)
+        {
+            owner.StopCoroutine(coroutine);
+            coroutine = null;
+        }
+    }
+	
+	#endregion
+	
+	#region Private Methods
+
+    private IEnumerator Process()
+    {
+        yield return routine.Invoke();
+        coroutine = null;
+
+        OnFinish?.Invoke();
+    }
+	
+	#endregion
+}
---
+++ Assets/Scripts/Core/ComponentFinder.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Extencions
+{
+    public static class ComponentFinder
+    {
+        /// <summary>
+        /// Вернет первый попавшийся на сцене объект типа T.
+        /// </summary>
+        public static T FindAtScene<T>() where T : Object
+        {
+            Log.Message($"Поиск объекта {typeof(T)} на сцене.");
+
+            T obj = default(T);
+            obj = GameObject.FindObjectOfType<T>();
+
+            if (obj == null)
+            {
+                Log.Error($"Искомый объект {typeof(T)} отсутствует на сцене!");
+                return null;
+            }
+
+            Log.Message($"Объект {typeof(T)} найден.");
+            return obj;
+        }
+		
+        /// <summary>
+        /// Вернет первый попавшийся в родительском объекте компонент типа T.
+        /// </summary>
+        public static T FindInParent<T>(GameObject gameObject) where T : Component
+        {
+            Log.Message($"Поиск компонента {typeof(T)} в родительском объекте.");
+
+            T obj = default(T);
+            obj = gameObject.GetComponentInParent<T>();
+
+            if (obj == null)
+            {
+                Log.Error($"Искомый объект {typeof(T)} отсутствует в родительском объекте!");
+                return null;
+            }
+
+            Log.Message($"Компонент {typeof(T)} найден.");
+            return obj;
+        }
+
+        /// <summary>
+        /// Вернет первый попавшийся в дочернем объекте компонент типа T.
+        /// </summary>
+        public static T FindInChildren<T>(GameObject gameObject) where T : Component
+        {
+            Log.Message($"Поиск компонента {typeof(T)} в дочернем объекте.");
+
+            T obj = default(T);
+            obj = gameObject.GetComponentInChildren<T>();
+
+            if (obj == null)
+            {
+                Log.Error($"Искомый объект {typeof(T)} отсутствует в дочернем объекте!");
+                return null;
+            }
+
+            Log.Message($"Компонент {typeof(T)} найден.");
+            return obj;
+        }
+
+        /// <summary>
+        /// Вернет первый попавшийся в объекте компонент типа T.
+        /// </summary>
+        public static T FindInSelf<T>(GameObject gameObject) where T : Component
+        {
+            Log.Message($"Поиск компонента {typeof(T)} в объекте.");
+
+            T obj = default(T);
+            obj = gameObject.GetComponent<T>();
+
+            if (obj == null)
+            {
+                Log.Error($"Искомый объект {typeof(T)} отсутствует в объекте!");
+                return null;
+            }
+
+            Log.Message($"Компонент {typeof(T)} найден.");
+            return obj;
+        }
+    }
+}
