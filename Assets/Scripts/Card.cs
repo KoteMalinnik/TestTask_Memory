1af13680cea7051ba5dbd23293b85555360fd461
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 08:02:32 +0300

    add Cards namespace

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,94 +1,97 @@
 ﻿using UnityEngine;
 using System;
 
-public class Card : MonoBehaviour
+namespace Cards
 {
-    #region Evenets
-
-    public static event Action<Card> OnShowed = null;
-    public static event Action<Card> OnHided = null;
-
-    #endregion
-
-    #region Serialized Fields
-
-    [SerializeField] private MeshRenderer imageRenderer = null;
-
-    #endregion
-
-    #region Properties
-
-    private bool IsOpen { get; set; } = false;
-
-    public string ImageName => imageRenderer?.sharedMaterial?.mainTexture?.name ?? "NotDefined";
-
-    #endregion
-
-    #region MonoBehaviour Callbacks
-
-    private void Awake()
+    public class Card : MonoBehaviour
     {
-        //установка начального вращения на случай, если префаб забудут повернуть на 90 градусов по оси X
-        FlipOver(openState: false);
-    }
+        #region Evenets
 
-    private void OnMouseDown()
-    {
-        Log.Message($"Нажатие на карту {name}");
+        public static event Action<Card> OnShowed = null;
+        public static event Action<Card> OnHided = null;
 
-        if (IsOpen == false) //открытие карты игроком должно происходить только один раз
+        #endregion
+
+        #region Serialized Fields
+
+        [SerializeField] private MeshRenderer imageRenderer = null;
+
+        #endregion
+
+        #region Properties
+
+        private bool IsOpen { get; set; } = false;
+
+        public string ImageName => imageRenderer?.sharedMaterial?.mainTexture?.name ?? "NotDefined";
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+        private void Awake()
         {
-            Show();
-        }
-    }
-
-    #endregion
-
-    #region Public Methods
-
-    public void SetMaterial(Material material)
-    {
-        if (material == null)
-        {
-            Log.Error("Материал не определена");
-            return;
+            //установка начального вращения на случай, если префаб забудут повернуть на 90 градусов по оси X
+            FlipOver(openState: false);
         }
 
-        Log.Message($"Установка материала карты {name}. Текстура: {material.mainTexture.name}");
+        private void OnMouseDown()
+        {
+            Log.Message($"Нажатие на карту {name}");
 
-        //imageRenderer.sharedMaterial.mainTexture = texture;
-        imageRenderer.material = material;
+            if (IsOpen == false) //открытие карты игроком должно происходить только один раз
+            {
+                Show();
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void SetMaterial(Material material)
+        {
+            if (material == null)
+            {
+                Log.Error("Материал не определена");
+                return;
+            }
+
+            Log.Message($"Установка материала карты {name}. Текстура: {material.mainTexture.name}");
+
+            //imageRenderer.sharedMaterial.mainTexture = texture;
+            imageRenderer.material = material;
+        }
+
+        public void Show()
+        {
+            Log.Message($"Открытие карты {name}");
+
+            FlipOver(true);
+
+            OnShowed?.Invoke(this);
+        }
+
+        public void Hide()
+        {
+            Log.Message($"Закрытие карты {name}");
+
+            FlipOver(false);
+
+            OnHided?.Invoke(this);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void FlipOver(bool openState)
+        {
+            float targetAngle = openState == true ? 270 : 90;
+            transform.localRotation = Quaternion.Euler(targetAngle, 0, 0);
+
+            IsOpen = openState;
+        }
+
+        #endregion
     }
-
-    public void Show()
-    {
-        Log.Message($"Открытие карты {name}");
-
-        FlipOver(true);
-
-        OnShowed?.Invoke(this);
-    }
-
-    public void Hide()
-    {
-        Log.Message($"Закрытие карты {name}");
-
-        FlipOver(false);
-
-        OnHided?.Invoke(this);
-    }
-
-    #endregion
-
-    #region Private Methods
-
-    private void FlipOver(bool openState)
-    {
-        float targetAngle = openState == true ? 270 : 90;
-        transform.localRotation = Quaternion.Euler(targetAngle, 0, 0);
-
-        IsOpen = openState;
-    }
-
-    #endregion
 }
--- Assets/Scripts/CardImagesSet.cs
+++ Assets/Scripts/CardImagesSet.cs
@@ -1,33 +1,36 @@
 ﻿using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewCardImagesSet", menuName = "Card Images Set")]
-public class CardImagesSet : ScriptableObject
+namespace Cards
 {
-    #region Serialize Fields
-
-    [SerializeField] private Texture[] imageTextures = new Texture[5];
-
-    #endregion
-
-    #region Properties
-
-    public int Length => imageTextures?.Length ?? 0;
-
-    #endregion
-
-    #region Public Methods
-
-    public Texture GetImage(int index)
+    [CreateAssetMenu(fileName = "NewCardImagesSet", menuName = "Card Images Set")]
+    public class CardImagesSet : ScriptableObject
     {
-        try
-        {
-            return imageTextures[index];
-        }
-        catch
-        {
-            return null;
-        }
-    }
+        #region Serialize Fields
 
-    #endregion
+        [SerializeField] private Texture[] imageTextures = new Texture[5];
+
+        #endregion
+
+        #region Properties
+
+        public int Length => imageTextures?.Length ?? 0;
+
+        #endregion
+
+        #region Public Methods
+
+        public Texture GetImage(int index)
+        {
+            try
+            {
+                return imageTextures[index];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        #endregion
+    }
 }
