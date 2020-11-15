248c4d816fc8ed3ae81aafd618fe9948ca196b4e
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 09:03:53 +0300

    add IChangeable interface with Value propertie and OnChanged event

---
+++ Assets/Scripts/IChangeable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public interface IChangeable
+{
+    #region Events
+
+    event Action<int> OnChanged;
+
+    #endregion
+
+    #region Properties
+
+    int Value { get; set; }
+
+    #endregion
+}
