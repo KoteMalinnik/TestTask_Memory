03cac038b7a1357586b5398be5b562853eda78e4
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 12:23:12 +0300

    add PlayingArea.cs
    Предназначен для слежения за картами на поле
    
    Содержит обработчики событий MovesChain.OnComplete и MovesChain.OnIncomplete.

---
+++ Assets/Scripts/PlayingArea.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System;
+using Player;
+
+namespace Cards
+{
+    public class PlayingArea : MonoBehaviour //контроль поведения открытых игроком карт
+    {
+        #region MonoBehaviour Callbacks
+
+        private void OnEnable()
+        {
+            MovesChain.OnCompleted += MovesChainCompletedEventHandler;
+            MovesChain.OnIncomplited += MovesChainIncompletedEventHandler;
+        }
+
+        private void OnDisable()
+        {
+            MovesChain.OnCompleted -= MovesChainCompletedEventHandler;
+            MovesChain.OnIncomplited -= MovesChainIncompletedEventHandler;
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void MovesChainCompletedEventHandler(Card[] cards)
+        {
+            Log.Message("Обработка события успешного завершения цепочки ходов");
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                Destroy(cards[i].gameObject); //Удаление карты с игрового поля
+            }
+        }
+
+        private void MovesChainIncompletedEventHandler(Card[] cards)
+        {
+            Log.Message("Обработка события неуспешного завершения цепочки ходов");
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                cards[i].Hide(); //переворачивание карты рубашкой вверх
+            }
+        }
+
+        #endregion
+    }
+}
