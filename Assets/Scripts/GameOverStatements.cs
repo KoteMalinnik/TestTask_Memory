a9c4a165f5908437878df1a338bcf45193c9636f
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 12:45:40 +0300

    add GameOverStatements with OnVictory and OnLoss events

---
+++ Assets/Scripts/GameOverStatements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class GameOverStatements //состояния конца игры (пародия на машину состояний)
+{
+    #region Events
+
+    public static event Action OnVictory = null;
+    public static event Action OnLoss = null;
+
+    #endregion
+
+    #region Public Methods
+
+    public static void EnterVictoryState()
+    {
+        Log.Message("Вход в состояние победы игрока");
+
+        OnVictory?.Invoke();
+    }
+
+    public static void EnteLossState()
+    {
+        Log.Message("Вход в состояние проигрыша игрока");
+
+        OnLoss?.Invoke();
+    }
+
+    #endregion
+}
