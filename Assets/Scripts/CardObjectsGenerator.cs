6021ab37cde1e605555ee0c304686c2ce7a7c48a
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 08:02:21 +0300

    move to SpawnPositions.cs spawn positions logic from CardObjectsGenerator.cs

--- Assets/Scripts/CardObjectsGenerator.cs
+++ Assets/Scripts/CardObjectsGenerator.cs
@@ -1,115 +1,91 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 
-public class CardObjectsGenerator : MonoBehaviour
+namespace Cards
 {
-    #region Serialize Fields
-
-    [Header("Параметры колод")]
-    [SerializeField] private int cardDeckCount = 5; //количество колод
-    [SerializeField] private int cardDeckCapacity = 3; //вместимость колоды
-
-    [Header("Параметры генерации")]
-    [SerializeField] private int raws = 5; //количество строк
-    [SerializeField] private int coloumns = 3; //количество столбцов
-    [SerializeField] private float offset = 0.2f; //отступ между соседними элементами
-    [Space]
-    [SerializeField] private Card cardPrefab = null; //префаб карты
-    [SerializeField] private Transform cardParentTransform = null; //родительский Transform сгенерированых карт
-    [SerializeField] private float spawnPositionY = 0.1f; //отступ по оси Y от нулевой координаты
-
-    [Header("Текстуры карт")]
-    [SerializeField] private CardImagesSet imagesSet = null; //сет текстур для карт
-    [SerializeField] private Material imageMaterial = null; //материал с текстурой
-
-    #endregion
-
-    private void Awake()
+    public class CardObjectsGenerator : MonoBehaviour
     {
-        Generate();
-    }
+        #region Serialize Fields
 
-    public Card[] Generate()
-    {
-        //проверка совпадения количества текстур с количеством колод
-        if (imagesSet.Length != cardDeckCount) 
+        [Header("Параметры колод")]
+        [SerializeField] private int cardDeckCount = 5; //количество колод
+        [SerializeField] private int cardDeckCapacity = 3; //вместимость колоды
+
+        [Header("Параметры генерации")]
+        [SerializeField] private int raws = 5; //количество строк
+        [SerializeField] private int coloumns = 3; //количество столбцов
+        [SerializeField] private float offset = 0.2f; //отступ между соседними элементами
+        [Space]
+        [SerializeField] private Card cardPrefab = null; //префаб карты
+        [SerializeField] private Transform cardParentTransform = null; //родительский Transform сгенерированых карт
+        [SerializeField] private float spawnPositionY = 0.1f; //отступ по оси Y от нулевой координаты
+
+        [Header("Текстуры карт")]
+        [SerializeField] private CardImagesSet imagesSet = null; //сет текстур для карт
+        [SerializeField] private Material imageMaterial = null; //материал с текстурой
+
+        #endregion
+
+        #region Public Methods
+        
+        public Card[] Generate()
         {
-            Log.Error($"Указанное количество колод в генераторе ({cardDeckCount}) " +
-                $"не совпадает с количеством текстур для колод ({imagesSet.Length})");
-
-            return null;
-        }
-
-        //проверка совпадения количества карт с размером игрового поля
-        if (raws * coloumns != cardDeckCount * cardDeckCapacity) 
-        {
-            Log.Error($"Общее количество карт во всех колодах ({cardDeckCount * cardDeckCapacity}) " +
-                $"не совпадает с расчетным количеством карт ({raws * coloumns}) " +
-                $"исходя из размеров игровго поля (строки: {raws}, столбцы: {coloumns})");
-
-            return null;
-        }
-
-        //Ось Х - вправо
-        //Ось Z - вверх
-        List<Vector3> spawnPositions = GetSpawnPositions();
-
-        List<Card> cards = new List<Card>();
-        for (int deskNumber = 0; deskNumber < cardDeckCount; deskNumber++)
-        {
-            Material material = new Material(imageMaterial);
-            material.mainTexture = imagesSet.GetImage(deskNumber);
-
-            for (int cardNumber = 0; cardNumber < cardDeckCapacity; cardNumber++)
+            //проверка совпадения количества текстур с количеством колод
+            if (imagesSet.Length != cardDeckCount)
             {
-                //выбираем случайное значение из индекса позиций и удаляем его из списка
-                int index = UnityEngine.Random.Range(0, spawnPositions.Count);
-                Vector3 spawnPosition = spawnPositions[index];
-                spawnPositions.RemoveAt(index);
+                Log.Error($"Указанное количество колод в генераторе ({cardDeckCount}) " +
+                    $"не совпадает с количеством текстур для колод ({imagesSet.Length})");
 
-                Card item = CreateCard(material, spawnPosition, deskNumber, cardNumber);
-                cards.Add(item);
+                return null;
             }
-        }
 
-        return cards.ToArray();
-    }
-
-    private List<Vector3> GetSpawnPositions()
-    {
-        Vector2 cardScale = cardPrefab.transform.localScale;
-
-        //отсуп на половину с учетом размера карты по оси: (var - cardScale) / 2
-        //учет отступа между элементами: (var - 1) * offset / 2
-        //итоговый отступ: (var - cardScale) / 2 + (var - 1) * offset / 2
-        float coloumnOffset = (coloumns - cardScale.x + (coloumns - 1) * offset) / 2;
-        float rawOffset = (raws - cardScale.y + (raws - 1) * offset) / 2;
-
-        Log.Message($"Смещение: по столбцу = {coloumnOffset}, по строке = {rawOffset}");
-
-        List<Vector3> spawnPositions = new List<Vector3>();
-        for (int raw = 0; raw < raws; raw++)
-        {
-            for (int coloumn = 0; coloumn < coloumns; coloumn++)
+            //проверка совпадения количества карт с размером игрового поля
+            if (raws * coloumns != cardDeckCount * cardDeckCapacity)
             {
-                //благодаря смещению центр сетки с картами будет находится в нулевой координате
-                float spawnPositionX = coloumn - coloumnOffset + coloumn * offset;
-                float spawnPositionZ = raw - rawOffset + raw * offset;
-                Vector3 spawnPosition = new Vector3(spawnPositionX, spawnPositionY, spawnPositionZ);
+                Log.Error($"Общее количество карт во всех колодах ({cardDeckCount * cardDeckCapacity}) " +
+                    $"не совпадает с расчетным количеством карт ({raws * coloumns}) " +
+                    $"исходя из размеров игровго поля (строки: {raws}, столбцы: {coloumns})");
 
-                spawnPositions.Add(spawnPosition);
+                return null;
             }
+
+            //Ось Х - вправо
+            //Ось Z - вверх
+            SpawnPositions spawnPositions = new SpawnPositions(cardPrefab.transform.localScale, coloumns, raws, offset, spawnPositionY);
+
+            List<Card> cards = new List<Card>();
+            for (int deskNumber = 0; deskNumber < cardDeckCount; deskNumber++)
+            {
+                Material material = new Material(imageMaterial);
+                material.mainTexture = imagesSet.GetImage(deskNumber);
+
+                for (int cardNumber = 0; cardNumber < cardDeckCapacity; cardNumber++)
+                {
+                    Vector3 spawnPosition;
+                    if (spawnPositions.TryGetPosition(out spawnPosition) == true)
+                    {
+                        Card item = CreateCard(material, spawnPosition, deskNumber, cardNumber);
+                        cards.Add(item);
+                    }
+                }
+            }
+
+            return cards.ToArray();
         }
 
-        return spawnPositions;
-    }
+        #endregion
 
-    private Card CreateCard(Material imageMaterial, Vector3 spawnPosition, int deskNumber, int cardNumber)
-    {
-        Card card = GameObject.Instantiate(cardPrefab, spawnPosition, Quaternion.identity, cardParentTransform);
-        card.SetMaterial(imageMaterial);
-        card.name = $"Card-{cardNumber}-{imageMaterial.mainTexture.name}";
+        #region Private Methods
 
-        return card;
+        private Card CreateCard(Material imageMaterial, Vector3 spawnPosition, int deskNumber, int cardNumber)
+        {
+            Card card = GameObject.Instantiate(cardPrefab, spawnPosition, Quaternion.identity, cardParentTransform);
+            card.SetMaterial(imageMaterial);
+            card.name = $"Card-{cardNumber}-{imageMaterial.mainTexture.name}";
+
+            return card;
+        }
+
+        #endregion
     }
 }
---
+++ Assets/Scripts/SpawnPositions.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Cards
+{
+    public class SpawnPositions
+    {
+        #region Properties
+
+        private List<Vector3> positions { get; } = null;
+
+        #endregion
+
+        #region Constructors
+
+        public SpawnPositions(Vector2 cardScale, int coloumns, int raws, float offset, float spawnPositionY)
+        {
+            positions = new List<Vector3>();
+
+            //отсуп на половину с учетом размера карты по оси: (var - cardScale) / 2
+            //учет отступа между элементами: (var - 1) * offset / 2
+            //итоговый отступ: (var - cardScale) / 2 + (var - 1) * offset / 2
+            float coloumnOffset = (coloumns - cardScale.x + (coloumns - 1) * offset) / 2;
+            float rawOffset = (raws - cardScale.y + (raws - 1) * offset) / 2;
+
+            Log.Message($"Смещение: по столбцу = {coloumnOffset}, по строке = {rawOffset}");
+
+            for (int raw = 0; raw < raws; raw++)
+            {
+                for (int coloumn = 0; coloumn < coloumns; coloumn++)
+                {
+                    //благодаря смещению центр сетки с картами будет находится в нулевой координате
+                    float spawnPositionX = coloumn - coloumnOffset + coloumn * offset;
+                    float spawnPositionZ = raw - rawOffset + raw * offset;
+                    Vector3 spawnPosition = new Vector3(spawnPositionX, spawnPositionY, spawnPositionZ);
+
+                    positions.Add(spawnPosition);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public bool TryGetPosition(out Vector3 position)
+        {
+            if (positions.Count == 0)
+            {
+                position = Vector3.zero;
+                return false;
+            }
+
+            //выбираем случайное значение из индекса позиций и удаляем его из списка
+            int index = UnityEngine.Random.Range(0, positions.Count);
+
+            position = positions[index];
+
+            positions.RemoveAt(index);
+
+            return true;
+        }
+
+        #endregion
+    }
+}
