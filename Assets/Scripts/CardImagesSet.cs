5427516dd4739e340857e5cfc5f27862f1fc77d7
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 05:22:42 +0300

    add card image set as scriptable object
    имеет 5 полей для текстур
    
    Получить длину - int Length
    Получить текстуру - Texture GetImage(int)

---
+++ Assets/Scripts/CardImagesSet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCardImagesSet", menuName = "Card Images Set")]
+public class CardImagesSet : ScriptableObject
+{
+    #region Serialize Fields
+
+    [SerializeField] private Texture[] imageTextures = new Texture[5];
+
+    #endregion
+
+    #region Properties
+
+    public int Length => imageTextures?.Length ?? 0;
+
+    #endregion
+
+    #region Public Methods
+
+    public Texture GetImage(int index)
+    {
+        try
+        {
+            return imageTextures[index];
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    #endregion
+}
