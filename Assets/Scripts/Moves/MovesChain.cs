83621a7f2122b44f66a5c558fda46b299ede482b
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 12:19:14 +0300

    add MovesChain.cs
    Скрипт цепочки ходов отвечает за ходы игрока.
    Колода для совпадений задается первой картой цепочки.
    
    Максимальное количество карт в цепочке можно задать статическим методом void SetMaxCardsCount(int)
    
    Добавить новую карту в цепочку можно с помощью метода void Append(Card)
    
    При совпадении карт в цепочке (кроме случая завершения цепочки) генерируется событие OnMatch.
    
    В момент достижения цепочкой предела ходов (предела количества карт в цепочке) генерируется событие OnComplete<Card[]>
    
    При любом несовпадении генерируется событие OnIncomplete<Card[]>

---
+++ Assets/Scripts/Moves/MovesChain.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System;
+using Cards;
+
+namespace Player
+{
+    public class MovesChain
+    {
+        #region Events
+
+        public static event Action<Card[]> OnCompleted = null; //цепочка успешна завершена
+        public static event Action<Card[]> OnIncomplited = null; //цепочка не завершилась (несовпадение)
+        
+        public event Action OnMatch = null; //успешное добавление карты в цепочку
+
+        #endregion
+
+        #region Properties
+
+        private static int MaxCardsCount { get; set; } = 3; //максимальное количество ходов
+
+        private List<Card> opendedCards { get; } = null; //открытые карты, прошедшие проверку на совпадение
+        private string DeckName { get; } = null; //имя колоды, карты которой проходят проверку
+
+        #endregion
+
+        #region Constructors
+
+        public MovesChain(Card initialCard)
+        {
+            Log.Message($"Создание цепочки ходов для колоды {initialCard.DeckName}");
+
+            DeckName = initialCard.DeckName; //первая карта задает целевое имя колоды
+
+            opendedCards = new List<Card>();
+            Add(initialCard); //добавляем карту в список без проверок
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        #region Static
+
+        public static void SetMaxCardsCount(int maxCardsCount)
+        {
+            if (maxCardsCount < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Log.Message($"Установка максимального количества карт в цепочке: {maxCardsCount}");
+
+            MaxCardsCount = maxCardsCount;
+        }
+
+        #endregion
+
+        public void Append(Card card)
+        {
+            Log.Message($"Попытка добавить карту {card.name} в цепочку ходов");
+
+            if (card.DeckName.Equals(DeckName))
+            {
+                Log.Message("Карты совпадают");
+
+                Add(card);
+
+                if (opendedCards.Count == MaxCardsCount)
+                {
+                    Log.Message("Цепочка ходов завершена");
+
+                    OnCompleted?.Invoke(opendedCards.ToArray());
+                }
+                else
+                {
+                    OnMatch?.Invoke();
+                }
+            }
+            else
+            {
+                Log.Message("Карты не совпадают");
+
+                OnIncomplited?.Invoke(opendedCards.ToArray());
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Add(Card card)
+        {
+            Log.Message($"Добавление карты {card.name} в цепочку ходов");
+
+            opendedCards.Add(card);
+
+            Log.Message($"Ход {opendedCards.Count} из {MaxCardsCount}");
+        }
+
+        #endregion
+    }
+}
