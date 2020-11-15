c22ae2ec3f7f250316a3c5cd44a134ae2da32e9f
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 09:20:27 +0300

    add player controller script
    Базовая реализация
    Контролирует количество жизней и очков.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region Serialize Fields
+
+        [SerializeField] private int lifesCount = 5;
+
+        #endregion
+
+        #region Properties
+
+        private Lifes Lifes { get; set; } = null;
+        private Score Score { get; set; } = null;
+
+        #endregion
+
+        #region MonoBehaviour Callbacks
+
+        //TODO: подписка на события изменения счета и жизней
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
+        private void OnApplicationQuit()
+        {
+            if (Score?.CheckNewBestScore() ?? false) //если Score не определен, то условие не выполняется
+            {
+                Score.UpdateBestScore(); //обновление лучшего счета
+                Score.SaveBestScore(); //сохранение лучшего счета
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Initialize()
+        {
+            Lifes = new Lifes(lifesCount);
+            Score = new Score();
+        }
+
+        #endregion
+    }
+}
