d3e8cc0cd5f38320fe4cb30fc97c5ab5ec994892
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 09:01:56 +0300

    add Serialization script that serialize int

---
+++ Assets/Scripts/Core/Serialization.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public static class Serialization
+{
+    public static void Save(string key, int value)
+    {
+        Log.Message($"Сериализация {typeof(int)} с ключом {key}. Значение: {value}");
+
+        PlayerPrefs.SetInt(key, value);
+    }
+
+    public static int Load(string key, int defaultValue)
+    {
+        if (PlayerPrefs.HasKey(key) == true)
+        {
+            int value = PlayerPrefs.GetInt(key, defaultValue);
+
+            Log.Message($"Десериализация {typeof(int)} с ключом {key}. Значение: {value}");
+
+            return value;
+        }
+        else
+        {
+            Save(key, defaultValue);
+            Load(key, defaultValue);
+
+            return defaultValue;
+        }
+    }
+}
