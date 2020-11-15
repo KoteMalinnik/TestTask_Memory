14c08aba5b8a02075ae82d82491907d10a3fb969
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 08:20:14 +0300

    add lifes controller (Lifes.cs)

---
+++ Assets/Scripts/Player/Lifes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Player
+{
+    public class Lifes
+    {
+        #region Events
+
+        public static event Action OnRunOut = null; //жизни закончились
+        public static event Action<int> OnDecrease = null; //жизни уменьшились
+
+        #endregion
+
+        #region Properties
+
+        public int Value { get; private set; } = 0;
+
+        #endregion
+
+        #region Constructors
+
+        public Lifes(int initialLifesCount)
+        {
+            if (initialLifesCount <= 0)
+            {
+                Log.Warning($"Передано неверное количество жизней ({initialLifesCount})");
+
+                initialLifesCount = 5; //по умолчанию будем выставлять 5 жизней, если неправильно подали аргумент
+            }
+
+            Log.Message($"Установка количества жизней: {initialLifesCount}");
+
+            Value = initialLifesCount;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Decrease()
+        {
+            if (Value == 0) //не надо вызвать какие-либо события, если жизни закончились
+            {
+                return;
+            }
+
+            Log.Message($"Уменшьение количества жизней ({Value}) -> ({Value - 1})");
+
+            Value--;
+            OnDecrease?.Invoke(Value);
+
+            if (Value == 0)
+            {
+                Log.Message("Жизни закончились");
+
+                OnRunOut?.Invoke();
+            }
+        }
+
+        #endregion
+    }
+}
