2fb82b1aea21dd52edd37ebe8e0bfb0aed217748
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-15 09:03:00 +0300

    add score controller (Score.cs)

---
+++ Assets/Scripts/Player/Score.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+namespace Player
+{
+    public class Score
+    {
+        #region Events
+
+        public static event Action<int> OnNewBest = null; //новый лучший счет
+        public static event Action<int> OnChanged = null; //количество очков изменилось
+
+        #endregion
+
+        #region Properties
+
+        private int Value { get; set; } = 0;
+        private int Best { get; set; } = 0;
+        
+        private string BestScoreKey => "BestScore";
+
+        #endregion
+
+        #region Constructors
+
+        public Score()
+        {
+            LoadBestScore();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Increase(int delta)
+        {
+            if (delta < 0)
+            {
+                Log.Warning($"Передано отрицательное изменение количества очков ({delta})");
+
+                return;
+            }
+
+            Log.Message($"Увеличение количества очков ({Value}) -> ({Value + delta})");
+
+            Value += delta;
+
+            OnChanged?.Invoke(Value);
+        }
+
+        public void UpdateBestScore()
+        {
+            Log.Message($"Достигнут новый лучший счет! ({Best}) -> ({Value})");
+
+            Best = Value;
+            OnNewBest?.Invoke(Best);
+        }
+
+        public bool CheckNewBestScore()
+        {
+            Log.Message($"Проверка на новый лучший счет. Результат проверки (Value > Best): {Value > Best}");
+
+            return Value > Best;
+        }
+            
+        public void SaveBestScore() => Serialization.Save(BestScoreKey, Best);
+        public void LoadBestScore() => Best = Serialization.Load(BestScoreKey, 0);
+
+        #endregion
+    }
+}
