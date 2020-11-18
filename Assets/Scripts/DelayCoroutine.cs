4856a09df71aaf99ff0adaa44eccd7163c9d636f
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-18 06:27:52 +0300

    add DelayCoroutine.cs

---
+++ Assets/Scripts/DelayCoroutine.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DelayCoroutine
+{
+    #region Properties
+
+    private CommonCoroutine DelayRoutine { get; } = null;
+    private float DelayInSeconds { get; } = 1;
+    
+    #endregion
+
+    public DelayCoroutine(MonoBehaviour owner, Action onFinish, float delayInSeconds)
+    {
+        DelayInSeconds = delayInSeconds;
+
+        DelayRoutine = new CommonCoroutine(owner, Delay);
+        DelayRoutine.OnFinish += onFinish;
+    }
+
+    public void Start()
+    {
+        DelayRoutine.Start();
+    }
+
+    private IEnumerator Delay()
+    {
+        Log.Message($"Начало задержки ({DelayInSeconds} сек)");
+
+        yield return new WaitForSeconds(DelayInSeconds);
+
+        Log.Message("Конец задержки");
+    }
+}
