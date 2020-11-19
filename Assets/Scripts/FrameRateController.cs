fb28e9b782bbbe7862b37505e1c03e0f92c0300d
Nikita Kalinnikov <niki9888985@mail.ru>
2020-11-19 11:18:20 +0300

    add frame rate controller

---
+++ Assets/Scripts/FrameRateController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class FrameRateController: MonoBehaviour
+{
+    #region Serialize Fields
+
+    [Range(30, 90)]
+    [SerializeField] private int frameRate = 60;
+
+    #endregion
+
+    #region Static Properties
+
+    private static bool IsInitialized { get; set; } = false;
+
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void Awake()
+    {
+        if (IsInitialized)
+        {
+            SelfDestroy();
+
+            return;
+        }
+
+        Application.targetFrameRate = frameRate;
+        IsInitialized = true;
+
+        SelfDestroy();
+    }
+
+    #endregion
+
+    private void SelfDestroy() => Destroy(gameObject);
+}
